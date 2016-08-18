99bf3e5a0bdd2aec17b3cb5c9bf298cc67cee50c
Zhou <zhouzhou1992@live.com>
2016-08-19 01:38:28 +0800

    git 未配置 、

--- src/main/java/CliTest.java
+++ src/main/java/CliTest.java
@@ -1,5 +1,9 @@
 
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.CommandLine;
 
 /**
  * Created by Zhou on 2016/8/18.
@@ -7,9 +11,26 @@ import org.apache.commons.cli.Options;
 public class CliTest {
 
     public static void main(String[] args) {
+        System.out.println(args.toString());
         Options options=new Options();
         options.addOption("t",false,"display current time");
         options.addOption("c", true, "country code");
+
+        CommandLineParser parser =new DefaultParser();
+        CommandLine commandLine=null;
+        try {
+            commandLine=parser.parse(options,args);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        if(commandLine.hasOption("c")){
+            String countryCode=commandLine.getOptionValue("c");
+            System.out.println(countryCode);
+        }
+        if(commandLine.hasOption("t")){
+            String currentTime=commandLine.getOptionValue("t");
+            System.out.println(currentTime);
+        }
     }
 
 
