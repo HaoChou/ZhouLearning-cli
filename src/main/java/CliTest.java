22fab3102011cfa5a79238503d62b5e6b42cf158
Zhou <zhouzhou1992@live.com>
2016-08-19 16:18:11 +0800

    加备注

--- src/main/java/CliTest.java
+++ src/main/java/CliTest.java
@@ -12,8 +12,10 @@ public class CliTest {
 
     public static void main(String[] args) {
         //java classNama -c ccc -t ttt 是会生成个数为4的数组 两辆一对 所以创建如下的数组
-        String[] argsTest=new String[]{"-c","ccc","-t","ttt"};
+        String[] argsTest=new String[]{"-c","ccc","-t","ttt"};//模拟命令行
         Options options=new Options();
+
+        //增加系统本身的设置
         options.addOption("t",false,"display current time");
         options.addOption("c", true, "country code");
 
@@ -22,8 +24,11 @@ public class CliTest {
         try {
             commandLine=parser.parse(options,argsTest);
         } catch (ParseException e) {
+            // 该异常会在输入了未知参数时抛出
+            // Unrecognized option: -s
             e.printStackTrace();
         }
+
         if(commandLine.hasOption("c")){
             String countryCode=commandLine.getOptionValue("c");
             System.out.println(countryCode);
@@ -32,6 +37,7 @@ public class CliTest {
             String currentTime=commandLine.getOptionValue("t");
             System.out.println(currentTime);//输出为null
         }
+
     }
 
 
